a5910519821fc64fb741ec008ab3d75d79467770
eminbasbayan <emin.basbayan@icloud.com>
2022-08-27 18:31:37 +0300

    tailwindcss variable ve font ayarları

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Index = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className="text-red-600 font-dancing text-center text-3xl kaybol">Feane</div>
+  );
 };
 
 export default Index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#00bcd4",
+        primary: "#ffbe33",
+        secondary: "#222831",
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
       },
     },
   },
