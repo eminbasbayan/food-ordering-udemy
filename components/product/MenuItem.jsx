d78f2ee474cf79a03df65264c713448729ee6579
eminbasbayan <emin.basbayan@icloud.com>
2022-08-29 22:25:23 +0300

    menu item component

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -1,12 +1,27 @@
 import Image from "next/image";
+import { RiShoppingCart2Fill } from "react-icons/ri";
 
 const MenuItem = () => {
   return (
-    <div>
-      <div className="relative w-40 h-40 z-50">
-        <Image src="/images/f1.png" alt="" layout="fill" />
+    <div className="bg-secondary rounded-3xl">
+      <div className="w-full  bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
+        <div className="relative w-36 h-36 hover:scale-110 transition-all">
+          <Image src="/images/f1.png" alt="" layout="fill" />
+        </div>
       </div>
-      <div></div>
+      <div className="p-[25px] text-white">
+        <h4 className="text-xl font-semibold">Delicious Pizza</h4>
+        <p className="text-[15px]">
+          Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit,
+          magnam voluptatem repellendus sed eaque
+        </p>
+        <div className="flex justify-between items-center mt-4">
+          <span>$20</span>
+          <button className="btn-primary !w-10 !h-10 !rounded-full !p-0 grid place-content-center">
+            <RiShoppingCart2Fill />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- components/product/MenuWrapper.jsx
+++ components/product/MenuWrapper.jsx
@@ -4,17 +4,25 @@ import MenuItem from "./MenuItem";
 
 const MenuWrapper = () => {
   return (
-    <div className="container mx-auto flex flex-col items-center mb-16">
-      <Title addClass="text-[40px]">Our Menu</Title>
-      <div className="mt-10">
-        <button className="px-6 py-2 bg-secondary rounded-3xl text-white">
-          All
-        </button>
-        <button className="px-6 py-2 rounded-3xl ">Burger</button>
-        <button className="px-6 py-2  rounded-3xl ">Pizza</button>
-        <button className="px-6 py-2  rounded-3xl ">Drink</button>
+    <div className="container mx-auto  mb-16">
+      <div className="flex flex-col items-center w-full">
+        <Title addClass="text-[40px]">Our Menu</Title>
+        <div className="mt-10">
+          <button className="px-6 py-2 bg-secondary rounded-3xl text-white">
+            All
+          </button>
+          <button className="px-6 py-2 rounded-3xl ">Burger</button>
+          <button className="px-6 py-2  rounded-3xl ">Pizza</button>
+          <button className="px-6 py-2  rounded-3xl ">Drink</button>
+        </div>
       </div>
-      <div className="mt-8">
+      <div className="mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
         <MenuItem />
       </div>
     </div>
