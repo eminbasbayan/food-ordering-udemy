093b8420134627cbf0caae5e26aeb7d3dc1883db
eminbasbayan <emin.basbayan@icloud.com>
2022-08-30 13:25:16 +0300

    reservation input düzenleme

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -3,21 +3,52 @@ import Input from "./form/Input";
 import Title from "./ui/Title";
 
 const Reservation = () => {
+  const inputs = [
+    {
+      id: 1,
+      name: "fullName",
+      type: "text",
+      placeholder: "Your Full Name",
+    },
+    {
+      id: 2,
+      name: "phoneNumber",
+      type: "number",
+      placeholder: "Your Phone Number",
+    },
+    {
+      id: 3,
+      name: "email",
+      type: "email",
+      placeholder: "Your Email Address",
+    },
+    {
+      id: 4,
+      name: "persons",
+      type: "number",
+      placeholder: "How Many Persons?",
+    },
+    {
+      id: 5,
+      name: "persons",
+      type: "datetime-local",
+      placeholder: "How Many Persons?",
+    },
+  ];
+
   return (
     <div className="container mx-auto py-12">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
-      <div className="flex justify-between flex-wrap gap-10">
+      <div className="flex justify-between flex-wrap-reverse gap-10">
         <div className="lg:flex-1 w-full">
           <div className="flex flex-col gap-y-3">
-            <Input />
-            <Input />
-            <Input />
-            <Input />
-            <Input />
+            {inputs.map((input) => (
+              <Input key={input.id} {...input} />
+            ))}
           </div>
           <button className="btn-primary mt-4">BOOK NOW</button>
         </div>
-        <div className="lg:flex-1 !h-[384px] w-full">
+        <div className="lg:flex-1 w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48348.66924008447!2d-74.24927437205034!3d40.766603131286395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c254b5958982c3%3A0xb6ab3931055a2612!2sEast%20Orange%2C%20New%20Jersey%2C%20Amerika%20Birle%C5%9Fik%20Devletleri!5e0!3m2!1str!2str!4v1661853137161!5m2!1str!2str"
             allowFullScreen=""
--- components/form/Input.jsx
+++ components/form/Input.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 
-const Input = () => {
+const Input = (props) => {
+  const { type, placeholder, ...inputProps } = props;
   return (
     <div className="w-full">
       <label className="relative block cursor-text w-full">
         <input
-          type="text"
-          className="h-14 w-full border border-primary outline-none px-4 peer pt-2"
+          type={type}
+          className={`h-14 w-full border border-primary outline-none px-4 peer ${
+            type !== "datetime-local" && "pt-2"
+          }`}
           required
+          {...inputProps}
         />
-        <span className="absolute top-0 left-0 px-4 text-sm flex items-center h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all">
-          Email
-        </span>
+        {type !== "datetime-local" && (
+          <span className="absolute top-0 left-0 px-4 text-sm flex items-center h-full peer-focus:h-7 peer-focus:text-xs peer-valid:h-7 peer-valid:text-xs transition-all">
+            {placeholder}
+          </span>
+        )}
       </label>
     </div>
   );
