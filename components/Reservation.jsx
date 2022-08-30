b1bbdd4bcb2c002d518004da622fc5f15b4ecbec
eminbasbayan <emin.basbayan@icloud.com>
2022-08-30 14:34:34 +0300

    react formik kurulumu

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -1,38 +1,59 @@
 import React from "react";
 import Input from "./form/Input";
 import Title from "./ui/Title";
+import { useFormik } from "formik";
 
 const Reservation = () => {
+  const onSubmit = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+    actions.resetForm();
+  };
+
+  const { values, handleSubmit, handleChange } = useFormik({
+    initialValues: {
+      fullName: "",
+      phoneNumber: "",
+      email: "",
+      persons: "",
+      date: "",
+    },
+    onSubmit,
+  });
+
   const inputs = [
     {
       id: 1,
       name: "fullName",
       type: "text",
       placeholder: "Your Full Name",
+      value: values.fullName,
     },
     {
       id: 2,
       name: "phoneNumber",
       type: "number",
       placeholder: "Your Phone Number",
+      value: values.phoneNumber,
     },
     {
       id: 3,
       name: "email",
       type: "email",
       placeholder: "Your Email Address",
+      value: values.email,
     },
     {
       id: 4,
       name: "persons",
       type: "number",
       placeholder: "How Many Persons?",
+      value: values.persons,
     },
     {
       id: 5,
-      name: "persons",
+      name: "date",
       type: "datetime-local",
-      placeholder: "How Many Persons?",
+      value: values.date,
     },
   ];
 
@@ -40,14 +61,16 @@ const Reservation = () => {
     <div className="container mx-auto py-12">
       <Title addClass="text-[40px] mb-10">Book A Table</Title>
       <div className="flex justify-between flex-wrap-reverse gap-10">
-        <div className="lg:flex-1 w-full">
+        <form className="lg:flex-1 w-full" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-y-3">
             {inputs.map((input) => (
-              <Input key={input.id} {...input} />
+              <Input key={input.id} {...input} onChange={handleChange} />
             ))}
           </div>
-          <button className="btn-primary mt-4">BOOK NOW</button>
-        </div>
+          <button className="btn-primary mt-4" type="submit">
+            BOOK NOW
+          </button>
+        </form>
         <div className="lg:flex-1 w-full">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48348.66924008447!2d-74.24927437205034!3d40.766603131286395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c254b5958982c3%3A0xb6ab3931055a2612!2sEast%20Orange%2C%20New%20Jersey%2C%20Amerika%20Birle%C5%9Fik%20Devletleri!5e0!3m2!1str!2str!4v1661853137161!5m2!1str!2str"
