65f8135ebc10e2df37baf385826986e95041aa4e
eminbasbayan <emin.basbayan@icloud.com>
2022-09-01 14:37:00 +0300

    profile page responsive

---
+++ components/profile/Account.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import Input from "../../components/form/Input";
+import Title from "../../components/ui/Title";
+import { useFormik } from "formik";
+import { profileSchema } from "../../schema/profile";
+
+const Account = () => {
+  const onSubmit = async (values, actions) => {
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+    actions.resetForm();
+  };
+
+  const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
+    useFormik({
+      initialValues: {
+        fullName: "",
+        phoneNumber: "",
+        email: "",
+        address: "",
+        job: "",
+        bio: "",
+      },
+      onSubmit,
+      validationSchema: profileSchema,
+    });
+  const inputs = [
+    {
+      id: 1,
+      name: "fullName",
+      type: "text",
+      placeholder: "Your Full Name",
+      value: values.fullName,
+      errorMessage: errors.fullName,
+      touched: touched.fullName,
+    },
+    {
+      id: 2,
+      name: "phoneNumber",
+      type: "number",
+      placeholder: "Your Phone Number",
+      value: values.phoneNumber,
+      errorMessage: errors.phoneNumber,
+      touched: touched.phoneNumber,
+    },
+    {
+      id: 3,
+      name: "email",
+      type: "email",
+      placeholder: "Your Email Address",
+      value: values.email,
+      errorMessage: errors.email,
+      touched: touched.email,
+    },
+    {
+      id: 4,
+      name: "address",
+      type: "text",
+      placeholder: "Your Address",
+      value: values.address,
+      errorMessage: errors.address,
+      touched: touched.address,
+    },
+    {
+      id: 5,
+      name: "job",
+      type: "text",
+      placeholder: "Your Job",
+      value: values.job,
+      errorMessage: errors.job,
+      touched: touched.job,
+    },
+    {
+      id: 6,
+      name: "bio",
+      type: "text",
+      placeholder: "Your Bio",
+      value: values.bio,
+      errorMessage: errors.bio,
+      touched: touched.bio,
+    },
+  ];
+  return (
+    <form className="lg:p-8 flex-1 lg:mt-0 mt-5">
+      <Title addClass="text-[40px]">Account Settings</Title>
+      <div className="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-4">
+        {inputs.map((input) => (
+          <Input
+            key="input.id"
+            {...input}
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+        ))}
+      </div>
+      <button className="btn-primary mt-4">Update</button>
+    </form>
+  );
+};
+
+export default Account;
--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,87 +1,14 @@
 import Image from "next/image";
-import Input from "../../components/form/Input";
-import Title from "../../components/ui/Title";
-import { useFormik } from "formik";
-import { profileSchema } from "../../schema/profile";
+
+import { useState } from "react";
+import Account from "../../components/profile/Account";
 
 const Profile = () => {
-  const onSubmit = async (values, actions) => {
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-    actions.resetForm();
-  };
+  const [tabs, setTabs] = useState(0);
 
-  const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
-    useFormik({
-      initialValues: {
-        fullName: "",
-        phoneNumber: "",
-        email: "",
-        address: "",
-        job: "",
-        bio: "",
-      },
-      onSubmit,
-      validationSchema: profileSchema,
-    });
-  const inputs = [
-    {
-      id: 1,
-      name: "fullName",
-      type: "text",
-      placeholder: "Your Full Name",
-      value: values.fullName,
-      errorMessage: errors.fullName,
-      touched: touched.fullName,
-    },
-    {
-      id: 2,
-      name: "phoneNumber",
-      type: "number",
-      placeholder: "Your Phone Number",
-      value: values.phoneNumber,
-      errorMessage: errors.phoneNumber,
-      touched: touched.phoneNumber,
-    },
-    {
-      id: 3,
-      name: "email",
-      type: "email",
-      placeholder: "Your Email Address",
-      value: values.email,
-      errorMessage: errors.email,
-      touched: touched.email,
-    },
-    {
-      id: 4,
-      name: "address",
-      type: "text",
-      placeholder: "Your Address",
-      value: values.address,
-      errorMessage: errors.address,
-      touched: touched.address,
-    },
-    {
-      id: 5,
-      name: "job",
-      type: "text",
-      placeholder: "Your Job",
-      value: values.job,
-      errorMessage: errors.job,
-      touched: touched.job,
-    },
-    {
-      id: 6,
-      name: "bio",
-      type: "text",
-      placeholder: "Your Bio",
-      value: values.bio,
-      errorMessage: errors.bio,
-      touched: touched.bio,
-    },
-  ];
   return (
-    <div className="flex px-10 min-h-[calc(100vh_-_433px)]">
-      <div className="w-80 flex-shrink-0">
+    <div className="flex px-10 min-h-[calc(100vh_-_433px)] lg:flex-row flex-col">
+      <div className="lg:w-80 w-100 flex-shrink-0">
         <div className="relative flex flex-col items-center px-10 py-5 border border-b-0">
           <Image
             src="/images/client2.jpg"
@@ -93,38 +20,45 @@ const Profile = () => {
           <b className="text-2xl mt-1">John Doe</b>
         </div>
         <ul className="text-center font-semibold">
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+          <li
+            className={`border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all ${
+              tabs === 0 && "bg-primary text-white"
+            }`}
+            onClick={() => setTabs(0)}
+          >
             <i className="fa fa-home"></i>
             <button className="ml-1 ">Account</button>
           </li>
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+          <li
+            className={`border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all ${
+              tabs === 1 && "bg-primary text-white"
+            }`}
+            onClick={() => setTabs(1)}
+          >
             <i className="fa fa-key"></i>
             <button className="ml-1">Password</button>
           </li>
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+          <li
+            className={`border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all ${
+              tabs === 2 && "bg-primary text-white"
+            }`}
+            onClick={() => setTabs(2)}
+          >
             <i className="fa fa-motorcycle"></i>
             <button className="ml-1">Orders</button>
           </li>
-          <li className="border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all">
+          <li
+            className={`border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all ${
+              tabs === 3 && "bg-primary text-white"
+            }`}
+            onClick={() => setTabs(3)}
+          >
             <i className="fa fa-sign-out"></i>
             <button className="ml-1">Exit</button>
           </li>
         </ul>
       </div>
-      <form className="p-8 flex-1">
-        <Title addClass="text-[40px]">Account Settings</Title>
-        <div className="grid grid-cols-2 gap-4 mt-4">
-          {inputs.map((input) => (
-            <Input
-              key="input.id"
-              {...input}
-              onBlur={handleBlur}
-              onChange={handleChange}
-            />
-          ))}
-        </div>
-        <button className="btn-primary mt-4">Update</button>
-      </form>
+      {tabs === 0 && <Account />}
     </div>
   );
 };
