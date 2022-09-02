9d5eedbcaf90e76e445fe6098c41af086ad5497b
eminbasbayan <emin.basbayan@icloud.com>
2022-09-02 13:37:45 +0300

    product detail fiyat hesaplama

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import Link from "next/link";
 import { RiShoppingCart2Fill } from "react-icons/ri";
 
 const MenuItem = () => {
   return (
     <div className="bg-secondary rounded-3xl">
       <div className="w-full  bg-[#f1f2f3] h-[210px] grid place-content-center rounded-bl-[46px] rounded-tl-2xl rounded-tr-2xl">
-        <div className="relative w-36 h-36 hover:scale-110 transition-all">
-          <Image src="/images/f1.png" alt="" layout="fill" />
-        </div>
+        <Link href="/product">
+          <div className="relative w-36 h-36 hover:scale-110 transition-all">
+            <Image src="/images/f1.png" alt="" layout="fill" />
+          </div>
+        </Link>
       </div>
       <div className="p-[25px] text-white">
         <h4 className="text-xl font-semibold">Delicious Pizza</h4>
--- pages/product/index.jsx
+++ pages/product/index.jsx
@@ -1,17 +1,63 @@
 import Image from "next/image";
-import React from "react";
+import { useState } from "react";
 import Title from "../../components/ui/Title";
 
+const itemsExtra = [
+  {
+    id: 1,
+    name: "Extra 1",
+    price: 1,
+  },
+  {
+    id: 2,
+    name: "Extra 2",
+    price: 2,
+  },
+  {
+    id: 3,
+    name: "Extra 3",
+    price: 3,
+  },
+];
+
 const Index = () => {
+  const [prices, setPrices] = useState([10, 20, 30]);
+  const [price, setPrice] = useState(prices[0]);
+  const [size, setSize] = useState(0);
+  const [extraItems, setExtraItems] = useState(itemsExtra);
+  const [extras, setExtras] = useState([]);
+
+  const handleSize = (sizeIndex) => {
+    const difference = prices[sizeIndex] - prices[size];
+    setSize(sizeIndex);
+    changePrice(difference);
+  };
+
+  const changePrice = (number) => {
+    setPrice(price + number);
+  };
+
+  const handleChange = (e, item) => {
+    const checked = e.target.checked;
+
+    if (checked) {
+      changePrice(item.price);
+      setExtras([...extras, item]);
+    } else {
+      changePrice(-item.price);
+      setExtras(extras.filter((extra) => extra.id !== item.id));
+    }
+  };
+  console.log(extras);
   return (
-    <div className="flex items-center h-screen gap-20 py-20 flex-wrap ">
-      <div className="relative md:flex-1 w-[80%] h-[80%] mx-20">
+    <div className="flex items-center md:h-[calc(100vh_-_88px)] gap-5 py-20 flex-wrap ">
+      <div className="relative md:flex-1 md:w-[80%] md:h-[80%] w-36 h-36 mx-auto">
         <Image src="/images/f1.png" alt="" layout="fill" objectFit="contain" />
       </div>
       <div className="md:flex-1 md:text-start text-center">
         <Title addClass="text-6xl">Good Pizza</Title>
         <span className="text-primary text-2xl font-bold underline underline-offset-1 my-4 inline-block">
-          $10
+          ${price}
         </span>
         <p className="text-sm my-4 md:pr-24">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
@@ -21,19 +67,28 @@ const Index = () => {
         <div>
           <h4 className="text-xl font-bold">Choose the size</h4>
           <div className="flex items-center gap-x-20 md:justify-start justify-center">
-            <div className="relative w-8 h-8">
+            <div
+              className="relative w-8 h-8 cursor-pointer"
+              onClick={() => handleSize(0)}
+            >
               <Image src="/images/size.png" alt="" layout="fill" />
               <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
                 Small
               </span>
             </div>
-            <div className="relative w-12 h-12">
+            <div
+              className="relative w-12 h-12 cursor-pointer"
+              onClick={() => handleSize(1)}
+            >
               <Image src="/images/size.png" alt="" layout="fill" />
               <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
                 Medium
               </span>
             </div>
-            <div className="relative w-16 h-16">
+            <div
+              className="relative w-16 h-16 cursor-pointer"
+              onClick={() => handleSize(2)}
+            >
               <Image src="/images/size.png" alt="" layout="fill" />
               <span className="absolute top-0 -right-6 text-xs bg-primary rounded-full px-[5px] font-medium">
                 Large
@@ -42,14 +97,16 @@ const Index = () => {
           </div>
         </div>
         <div className="flex gap-x-4 my-6 md:justify-start justify-center">
-          <label className="flex items-center gap-x-1">
-            <input type="checkbox" className="w-5 h-5 accent-primary" />
-            <span className="text-sm font-semibold">ketçap</span>
-          </label>
-          <label className="flex items-center gap-x-1">
-            <input type="checkbox" className="w-5 h-5 accent-primary" />
-            <span className="text-sm font-semibold">ketçap</span>
-          </label>
+          {extraItems.map((item) => (
+            <label className="flex items-center gap-x-1" key={item.id}>
+              <input
+                type="checkbox"
+                className="w-5 h-5 accent-primary"
+                onChange={(e) => handleChange(e, item)}
+              />
+              <span className="text-sm font-semibold">{item.name}</span>
+            </label>
+          ))}
         </div>
         <button className="btn-primary">Add to Cart</button>
       </div>
