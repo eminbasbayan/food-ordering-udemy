3ceedfda64d6fe319dc34ddfc758c18a5e48fc6b
eminbasbayan <emin.basbayan@icloud.com>
2022-09-06 13:39:33 +0300

    github veri tabanı kayıt

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,10 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "../../../util/mongo";
 
 export default NextAuth({
-  // Configure one or more authentication providers
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
