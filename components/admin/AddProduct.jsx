f2be8e8d7f9f060ec23b65548cee183013941e5c
eminbasbayan <emin.basbayan@icloud.com>
2022-09-07 19:02:04 +0300

    cloudinary ayarları

--- components/admin/AddProduct.jsx
+++ components/admin/AddProduct.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import OutsideClickHandler from "react-outside-click-handler";
 import Title from "../ui/Title";
 import { GiCancel } from "react-icons/gi";
+import axios from "axios";
 
 const AddProduct = ({ setIsProductModal }) => {
   const [file, setFile] = useState();
@@ -16,7 +17,21 @@ const AddProduct = ({ setIsProductModal }) => {
     };
 
     reader.readAsDataURL(changeEvent.target.files[0]);
-    console.log(imageSrc);
+  };
+
+  const handleCreate = async () => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "food-ordering");
+
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/bilgisayar-genetigi/image/upload",
+        data
+      );
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -119,7 +134,12 @@ const AddProduct = ({ setIsProductModal }) => {
               </div>
             </div>
             <div className="flex justify-end">
-              <button className="btn-primary !bg-success ">Create</button>
+              <button
+                className="btn-primary !bg-success"
+                onClick={handleCreate}
+              >
+                Create
+              </button>
             </div>
             <button
               className="absolute  top-4 right-4"
