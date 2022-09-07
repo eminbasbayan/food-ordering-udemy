bce9ac2b37e6353bdbfd64ab83cb46de37ed9f51
eminbasbayan <emin.basbayan@icloud.com>
2022-09-07 15:36:54 +0300

    product get ve post işlemleri

---
+++ pages/api/products/index.js
@@ -0,0 +1,27 @@
+import Product from "../../../models/Product";
+import dbConnect from "../../../util/dbConnect";
+
+const handler = async (req, res) => {
+  await dbConnect();
+  const { method } = req;
+
+  if (method === "GET") {
+    try {
+      const products = await Product.find();
+      res.status(200).json(products);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (method === "POST") {
+    try {
+      const newProduct = await Product.create(req.body);
+      res.status(200).json(newProduct);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+export default handler;
