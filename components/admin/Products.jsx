3903a6b59a9711d649b7f5b4a1240be4d3aea1bb
eminbasbayan <emin.basbayan@icloud.com>
2022-09-07 22:47:49 +0300

    product delete işlemi

--- components/admin/Products.jsx
+++ components/admin/Products.jsx
@@ -1,7 +1,43 @@
 import Title from "../ui/Title";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const handleDelete = async (id) => {
+    try {
+      if (confirm("Are you sure you want to delete this product?")) {
+        const res = await axios.delete(
+          `${process.env.NEXT_PUBLIC_API_URL}/products/${id}`
+        );
+        if (res.status === 200) {
+          toast.success("Product Deleted!");
+          getProducts();
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getProducts = async () => {
+    try {
+      const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/products`
+      );
+      setProducts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className="lg:p-8 flex-1 lg:mt-0 mt-5">
       <Title addClass="text-[40px]">Products</Title>
@@ -27,23 +63,39 @@ const Products = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className="transition-all bg-secondary border-gray-700 hover:bg-primary ">
-              <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
-                <Image src="/images/f1.png" alt="" width={50} height={50} />
-              </td>
-              <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                63049e92...
-              </td>
-              <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                Good Pizza
-              </td>
-              <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                $ 10
-              </td>
-              <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
-                <button className="btn-primary !bg-danger">Delete</button>
-              </td>
-            </tr>
+            {products.length > 0 &&
+              products.map((product) => (
+                <tr
+                  className="transition-all bg-secondary border-gray-700 hover:bg-primary"
+                  key={product._id}
+                >
+                  <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
+                    <Image
+                      src={product.img}
+                      alt={product.title}
+                      width={50}
+                      height={50}
+                    />
+                  </td>
+                  <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    {product._id.substring(0, 5)}...
+                  </td>
+                  <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    {product.title}
+                  </td>
+                  <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    $ {product.prices[0]}
+                  </td>
+                  <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
+                    <button
+                      className="btn-primary !bg-danger"
+                      onClick={() => handleDelete(product._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
---
+++ pages/api/products/[id].js
@@ -0,0 +1,30 @@
+import Product from "../../../models/Product";
+import dbConnect from "../../../util/dbConnect";
+
+const handler = async (req, res) => {
+  await dbConnect();
+  const {
+    method,
+    query: { id },
+  } = req;
+
+  if (method === "GET") {
+    try {
+      const product = await Product.findById(id);
+      res.status(200).json(product);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (method === "DELETE") {
+    try {
+      const product = await Product.findByIdAndDelete(id);
+      res.status(200).json(product);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+export default handler;
