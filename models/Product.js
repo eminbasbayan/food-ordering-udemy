64e9c32d984a2983aac9cf06753cf590e9a0499c
eminbasbayan <emin.basbayan@icloud.com>
2022-09-07 15:10:50 +0300

    product model oluşturma

---
+++ models/Product.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxlength: 60,
+    },
+    desc: {
+      type: String,
+      required: true,
+      maxlength: 300,
+    },
+    prices: {
+      type: [Number],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    extraOptions: {
+      type: [
+        {
+          text: { type: String },
+          price: { type: Number },
+        },
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Product ||
+  mongoose.model("Product", ProductSchema);
