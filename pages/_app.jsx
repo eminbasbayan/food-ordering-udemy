8865a1e3fbc5cb1e378e3951391f8e604c97936e
eminbasbayan <emin.basbayan@icloud.com>
2022-09-08 17:50:38 +0300

    route change progress kurulumu

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,22 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import "../styles/globals.css";
 import Layout from "../layout/Layout";
-
-import { Provider } from "react-redux";
 import store from "../redux/store";
+import Router from "next/router";
+import NProgress from "nprogress";
 
 import { SessionProvider } from "next-auth/react";
-
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+
+import "../styles/globals.css";
+import "nprogress/nprogress.css";
 import "react-toastify/dist/ReactToastify.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
